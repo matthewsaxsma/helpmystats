a3799796e2ad0d79aabfe37a6238b1b418f5120f
Matthew Saxsma <matthewg.saxsma@gmail.com>
2025-08-05 16:52:38 -0500

    smol changes

--- helpmystats.R
+++ helpmystats.R
@@ -1,11 +1,11 @@
 # source packages --------------------------------------------------------------
-source("helpmystats_P.R")
+source("helpmystats_packages.R")
 
 # user interface ---------------------------------------------------------------
-source("helpmystats_UI.R")
+source("helpmystats_ui.R")
 
 # server -----------------------------------------------------------------------
-source("helpmystats_SERVER.R")
+source("helpmystats_server.R")
 
 # ShinyApp function ------------------------------------------------------------
 shinyApp(ui = ui, server = server, options = list(height = "750px"))
